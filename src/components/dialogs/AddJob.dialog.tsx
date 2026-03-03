08962bbe5957b927228ef853a0e293863cc22f3c
Vatukah <anupamthakur058@gmail.com>
2026-03-03 21:54:09 +0530

    MVP ready2

--- src/components/dialogs/AddJob.dialog.tsx
+++ src/components/dialogs/AddJob.dialog.tsx
@@ -5,30 +5,48 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 import { Button } from "../ui/button";
 
 import Icon from "../Icon";
-import AddJobForm from "../form/addJob.form.tsx";
+import AddJobForm from "@/components/form/addJob.form";
 
-
-function AddJobDialog({label,variant,icon}:{label:string,variant:"link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | null | undefined,icon:string}){
-
-    return(
+function AddJobDialog({
+  label,
+  variant,
+  icon,
+}: {
+  label: string;
+  variant:
+    | "link"
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | null
+    | undefined;
+  icon: string;
+}) {
+  return (
     <Dialog>
-  <DialogTrigger asChild><Button variant={variant} ><Icon iconName={icon} /> <span>{label}</span></Button></DialogTrigger>
-  
-  <DialogContent  className="h-[calc(100vh-64px)]">
-    <DialogHeader>
-      <DialogTitle className="text-xl">Add New Job</DialogTitle>
-     <DialogDescription>
-      Fill out the form below to create a new job listing.
-    </DialogDescription>
-    </DialogHeader>
- <AddJobForm/>
-  </DialogContent>
-</Dialog>
-    )
+      <DialogTrigger asChild>
+        <Button variant={variant}>
+          <Icon iconName={icon} /> <span>{label}</span>
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent className="h-[calc(100vh-64px)]">
+        <DialogHeader>
+          <DialogTitle className="text-xl">Add New Job</DialogTitle>
+          <DialogDescription>
+            Fill out the form below to create a new job listing.
+          </DialogDescription>
+        </DialogHeader>
+        <AddJobForm />
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default AddJobDialog;
